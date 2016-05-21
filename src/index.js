b1eef7e4a5078eaad4da5ae0bbe1c0a666fc4c85
rdmclin2 <rdmclin2@163.com>
2016-05-21 17:16:14 +0800

    add basic abstrct && add test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
-export default function foo() {
-  return 'foo'
+import adb from 'adbkit'
+import os from 'os'
+import fs from 'fs'
+
+const client = adb.createClient()
+
+class Device {
+  constructor(deviceId) {
+    this.deviceId = deviceId
+  }
+
+  adbshell(cmd) {
+    return new Promise((resolve, reject) => {
+      client.shell(this.deviceId, cmd)
+      .then(adb.util.readAll)
+      .then((output) => { resolve(output.toString().trim()) })
+      .catch((err) => { reject(err) })
+    })
+  }
+
+  async getFocusedPackageAndActivity() {
+    const reg = /[a-zA-Z0-9.]+\/.[a-zA-Z0-9.]+/
+    const cmd = 'dumpsys input | grep FocusedApplication'
+    const output = await this.adbshell(cmd)
+    return output.match(reg)[0]
+  }
+
+  async getCurrentPackageName() {
+    const pkgact = await this.getFocusedPackageAndActivity()
+    return pkgact.split('/')[0]
+  }
+
+  async getCurrentActivity() {
+    const pkgact = await this.getFocusedPackageAndActivity()
+    return pkgact.split('/')[1]
+  }
+
+  /**
+   * Dump ui xml and pull it to local temp file
+   * @return Promise [resolve the target xml file]
+   */
+  async dumpUI() {
+    const source = '/storage/sdcard0/window_dump.xml'
+    const cmd = `uiautomator dump ${source}`
+    const target = `${os.tmpdir()}/dumpfile.xml`
+    await client.shell(this.deviceId, cmd)
+    const output = await this.pullFile(source, target)
+    return output
+  }
+
+  async startActivity(componet) {
+    const cmd = `am start -S -W -n ${componet}`
+    await client.shell(this.deviceId, cmd)
+  }
+
+  pullFile(source, target) {
+    return new Promise((resolve, reject) => {
+      client.pull(this.deviceId, source)
+      .then((transfer) => {
+        transfer.on('end', () => resolve(target))
+        transfer.on('error', reject)
+        transfer.pipe(fs.createWriteStream(target))
+      })
+    })
+  }
 }
+
+export default Device
+
+/**
+ * tests
+ */
+
+// const deviceId = 'DU2SSE1478031311'
+// const device = new Device(deviceId)
+
+// device.dumpUI().then((output) => console.log(output))
+// device.getFocusedPackageAndActivity().then((output) => console.log(output))
+// device.getCurrentActivity().then((act) => console.log(act))
+// device.getCurrentPackageName().then((pkg) => console.log(pkg))
+// device.startActivity('com.cvicse.zhnt/.LoadingActivity').then(() => {
+//   console.log('should start the activity')
+// })
+
--- test/test.js
+++ test/test.js
@@ -1,10 +1,24 @@
-import foo from '../'
+import Device from '../'
 import assert from 'assert'
 
-describe('node-es6-template', () => {
-  it('should return foo', done => {
-    const output = foo()
-    assert(output, 'foo')
+describe('testing device api', () => {
+  const deviceId = 'DU2SSE1478031311'
+  let device
+
+  before(() => {
+    device = new Device(deviceId)
+  })
+
+  it('should startActivity ', async done => {
+    const pkg = 'com.cvicse.zhnt'
+    const act = '.LoadingActivity'
+
+    const componet = `${pkg}/${act}`
+    console.log(componet)
+
+    await device.startActivity(componet)
+    const output = await device.getCurrentPackageName(deviceId)
+    assert.equal(output, pkg)
     done()
   })
 })
